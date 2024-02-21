0af2be1184192c92c500da90a302ef2a70e5233c
Flávia Reis <flaviarster@gmail.com>
2024-02-21 11:26:33 -0300

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ibm': ['IBM Plex Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
