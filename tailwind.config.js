35de0f8fe6588dafa643557124489f3ea74117de
Flávia Reis <flaviarster@gmail.com>
2024-02-21 12:55:27 -0300

    Doing the styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         'ibm': ['IBM Plex Sans', 'sans-serif'],
       },
+      backgroundImage: {
+        'bg-mobile': "url('../assets/bg-main-mobile.png')",
+        'bg-tablet': "url('../assets/bg-main-tablet.png')",
+        'bg-desktop': "url('../assets/bg-main-desktop.png')",
+        'pattern-1': "url('../assets/bg-pattern-1.svg')",
+        'pattern-2': "url('../assets/bg-pattern-2.svg')",
+      },
     },
   },
   plugins: [],
